3ad9772af096324217a9f79a42d1ef710ebfbc04
Achraf Antri <74018091+achrafantri@users.noreply.github.com>
2025-09-18 12:50:35 +0100

    Delete Desktop/Nouveau dossier (2)/achats directory

--- Desktop/Nouveau dossier (2)/achats/src/main/java/com/example/achats/repository/BonDeCommandeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.achats.repository;
-
-import com.example.achats.model.BonDeCommande;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BonDeCommandeRepository extends JpaRepository<BonDeCommande, Long> {
-    // Méthodes personnalisées peuvent être ajoutées ici si nécessaire
-}
